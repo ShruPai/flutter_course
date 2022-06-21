67d6aa7b98ec82db4d9a3a532454d17db4d2da56
ShruPai <shruthipai1924@gmail.com>
2022-06-21 17:19:26 +0530

    Starting journey with flutter

---
+++ i_am_poor/lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+  MaterialApp(
+    home:Scaffold(
+      appBar:AppBar(
+        title:Center(
+          child:Text('I am poor')
+        ),
+        backgroundColor: Colors.pinkAccent,
+      ),
+      backgroundColor: Colors.blueAccent,
+      body: Center(
+        child: Text('I need to start working'),
+      ),
+      ),
+    ),
+);
+
